075ba0cb4c380f5dfed24ff2c28acd5cb81a577e
Philip Yap <philipyap@Philips-MacBook.local>
2020-08-03 23:07:44 -0700

    [node-module-practice]

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const myModule = require('./myModule.js')
+
+console.log(myModule.favFood())
+
+var oneLinerJoke = require('one-liner-joke');
+
+var getRandomJoke = oneLinerJoke.getRandomJoke();
+console.log(getRandomJoke)
---
+++ myModule.js
@@ -0,0 +1,6 @@
+module.exports.favFood =()=> ['pizza', 'burger', 'friedrice']
+
+module.exports.array =() => {
+    for (let i = 0; i<= 2; i++)
+    console.log(i)
+}
