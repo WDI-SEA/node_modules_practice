c3d4005b9be8312867719afdce20df563e595a87
imjchiang <jchiang296@gmail.com>
2020-08-04 08:48:54 -0700

    [JS] implement chalk package

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const chalk = require("chalk");
 
 console.log(chalk.green("Cool beans bro"));
+console.log(chalk.red("You have a fake error!"));
+console.log(chalk.blue("This is the coolest color"));
+console.log(chalk.yellow("This is a bright color"));
+console.log(chalk.red(`I ${chalk.yellow("am")} ${chalk.green("way")} ${chalk.blue("too")} ${chalk.white("cool")} ${chalk.gray("for")} you`));
+
