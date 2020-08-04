919ba22575271c6a7e4b9dd6ac3f43fab48f5bd1
Elaine <elaine@Felipes-iMac.local>
2020-08-03 21:35:21 -0500

    [npm] uses npm to console log colors of foods

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const nodeModule = require('./node_modules.js')
-nodeModule.favoriteFoodsLoop()
+const foodModule = require('./foodModule.js');
+const chalk = require('chalk');
+foodModule.favoriteFoodsLoop();
+console.log(chalk.hex('#52a363')(foodModule.favoriteFoods[0]))
+console.log(chalk.hex('#EDADAA')(foodModule.favoriteFoods[1]))
+console.log(chalk.hex('#8A9A5B')(foodModule.favoriteFoods[2]))
+console.log(chalk.hex('#E3FF00')(foodModule.favoriteFoods[3]))
+console.log(chalk.hex('#FFA600')(foodModule.favoriteFoods[4]))
+console.log(chalk.hex('#DE3163')(foodModule.favoriteFoods[5]))
