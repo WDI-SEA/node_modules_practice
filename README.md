9a76a59dc132ba93423a16cc0952f8abe5ecfddf
Lizz <lizwesterband@gmail.com>
2020-08-04 08:53:17 -0700

    [add] Add finished assignment and repo link

--- README.md
+++ README.md
@@ -19,6 +19,9 @@
 * add notes about how to create and import node modules to your Node/Express app readme, along with instructions about adding a `.gitignore`
 * paste the link to your repo in this the readme of this assignment
 
+** Link to repo **
+https://github.com/Lizzwest/Second-Node-Project
+
 ## Finally, .gitignore and submit a PR!
 
 * add a `.gitignore` to this assignment, so the `node_modules` folder doesn't upload to github
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+
+
+
 const myModule = require('./myModules.js')
+
 
 const faveMeals = (meals) => {
     for(let i=0;i<meals.length;i++) {
@@ -10,3 +14,13 @@ faveMeals(myModule.favoriteFoods)
 
 //I installed gitsnap, which supposedly takes a 
 //selfie on each commit. So we shall see if one ends up in my gitsnap folder!
+//**After commiting and pushing, I am not seeing anything in the gitsnap folder */
+
+//Second attempt. I installed chalk-animation.
+//It creates an animated text in the style of your choice. 
+//I LOVE IT!
+
+
+const chalkAnimation = require('chalk-animation');
+ 
+chalkAnimation.rainbow('Show me the rainbow');
