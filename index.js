15e847077b97e134992329d2983bf16347f7846c
margare-jihua <margaret.jihua@gmail.com>
2020-08-03 19:11:00 -0700

    Add superheroes npm

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const food = myModule.favoriteFood()
 for (let i = 0; i < food.length; i++) {
     console.log(food[i]);
 }
+
+/*----------npm--------*/
+
+const superheroes = require('superheroes')
+console.log('My Super Hero: ' + superheroes.random())
